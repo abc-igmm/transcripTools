d50668b0c887a01e0aa5f921f4880ce301514219
pearcedom <dominic.pearce@ed.ac.uk>
2018-05-28 11:59:52 +0100

    added nki data and started vignette

---
+++ R/nki.R
@@ -0,0 +1,15 @@
+#' NKI invasive breast cancer expressionSet
+#'
+#' Originally used as a[validation dataset for the 70-gene MammaPrint signature
+#' (https://www.ncbi.nlm.nih.gov/pubmed/12490681) this dataset has grown 
+#' slightly in size but still is effectively the same collection of invasive 
+#' breast cancer samples of no specific subtype. More information can be found 
+#' in the bioconductor package's web page but, because there are issues with 
+#' installing this on windows I've simply included the data here as part of 
+#' transcripTools. 
+#'
+#' @format An expressionSet with 337 samples and 24481 features, as well as 
+#' phenotypic and feature data
+#'
+#' @source \url{http://bioconductor.org/packages/release/data/experiment/html/breastCancerNKI.html}
+"nki"
